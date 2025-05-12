44c04b41d5f44b5a49b7e3d20f9f676e7e45d15e
s31725 <s31725@pjwstk.edu.pl>
2025-05-12 14:25:51 +0200

    feat: added DTOs

---
+++ apbd-test1/Models/DTOs/AddAppointmentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_test1.Models.DTOs;
+
+public class AddAppointmentDTO
+{
+    public int AppointmentId { get; set; }
+    public int PatientId { get; set; }
+    public string PWZ { get; set; }
+    public List<ServiceDTO> Services { get; set; }
+}
---
+++ apbd-test1/Models/DTOs/AppointmentDetailsDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace apbd_test1.Models.DTOs;
+
+public class AppointmentDetailsDTO
+{
+    public DateTime Date { get; set; }
+    public PatientDTO Patient { get; set; }
+    public DoctorDTO Doctor { get; set; }
+    public List<ServiceDTO> AppointmentServices { get; set; }
+}
+
+public class PatientDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
+
+public class DoctorDTO
+{
+    public int DoctorId { get; set; }
+    public string PWZ { get; set; }
+}
+
+public class ServiceDTO
+{
+    public int Name { get; set; }
+    public decimal ServiceFee { get; set; }
+}
