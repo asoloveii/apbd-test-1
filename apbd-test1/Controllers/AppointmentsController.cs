2266aa0359482b1b8f076bd48aee6c3570e1c4ac
s31725 <s31725@pjwstk.edu.pl>
2025-05-12 15:09:30 +0200

    feat: application's main controller

---
+++ apbd-test1/Controllers/AppointmentsController.cs
@@ -0,0 +1,49 @@
+﻿using apbd_test1.Exceptions;
+using apbd_test1.Models.DTOs;
+using apbd_test1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_test1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public AppointmentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAppointmentDetails(int id)
+        {
+            try
+            {
+                var res = await _dbService.GetAppointmentDetailsByIdAsync(id);
+                return Ok(res);
+            }
+            catch (NotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAppointment([FromBody] AddAppointmentDTO appointmentDTO)
+        {
+            try
+            {
+                await _dbService.AddAppointmentAsync(appointmentDTO);
+                return CreatedAtAction(nameof(GetAppointmentDetails), new { appointmentDTO.AppointmentId }, null);
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
+
